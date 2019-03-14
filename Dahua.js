2d310a8f9a2c38b5e911e42fbc048f336bb62b77
taige hong <taigehong@taiges-mbp.attlocal.net>
2019-03-13 22:47:21 -0500

    some changes

--- Dahua.js
+++ Dahua.js
@@ -1,18 +1,9 @@
 // Create your global variables below:
-var timechosen1 = 0;
-var timechosen2 = 0;
-var timechosen3 = 0;
-var timechosen4 = 0;
-var timechosen5 = 0;
-var timechosen6 = 0;
-var timechosen7 = 0;
-var timechosen8 = 0;
-var timechosen9 = 0;
-var timechosen10 = 0;
-var timechosen11 = 0;
-var timechosen12 = 0;
-var am = 0;
-var pm = 0;
+var timechosen = 0;
+var pm = 2;
+var timechanged = 0;
+var time;
+var options = {year: 'numeric', month: '2-digit', day: 'numeric', hour: '2-digit', minute: '2-digit' };
 
 function secondsToMs(d) {
     d = Number(d);
@@ -23,175 +14,147 @@ function secondsToMs(d) {
     return `0${min}`.slice(-1) + ":" + `00${sec}`.slice(-2);
 }
 
-init();
-
-
+function changed(){
+    timechanged = 1;
+    checkIfComplete();
+}
 
 function choosetime1(){
-  if (timechosen1 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-1").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen1 = 1;
-	}
-	else if (timechosen1 ==1){
-		document.getElementById("time-button-1").style.backgroundColor = "pink";
-		timechosen1 = 0;
-  }
+    reset();
+    document.getElementById("time-button-1").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 1;
+    checkIfComplete();
 }
 
 function choosetime2(){
-	if (timechosen2 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-2").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen2 = 1;
-	}
-	else if (timechosen2 ==1){
-		document.getElementById("time-button-2").style.backgroundColor = "pink";
-		timechosen2 = 0;
-  }
+    reset();
+    document.getElementById("time-button-2").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 2;
+    checkIfComplete();
 }
 
 function choosetime3(){
-	if (timechosen3 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-3").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen3 = 1;
-	}
-	else if (timechosen3 ==1){
-		document.getElementById("time-button-3").style.backgroundColor = "pink";
-		timechosen3 = 0;
-  }
+    reset();
+    document.getElementById("time-button-3").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 3;
+    checkIfComplete();
 }
 
 function choosetime4(){
-	if (timechosen4 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-4").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen4 = 1;
-	}
-	else if (timechosen4 ==1){
-		document.getElementById("time-button-4").style.backgroundColor = "pink";
-		timechosen4 = 0;
-  }
+    reset();
+    document.getElementById("time-button-4").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 4;
+    checkIfComplete();
 }
 
 function choosetime5(){
-	if (timechosen5 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-5").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen5 = 5;
-	}
-	else if (timechosen5 ==1){
-		document.getElementById("time-button-5").style.backgroundColor = "pink";
-		timechosen5 = 0;
-  }
+    reset();
+    document.getElementById("time-button-5").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 5;
+    checkIfComplete();
 }
 
 function choosetime6(){
-	if (timechosen6 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-6").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen6 = 1;
-	}
-	else if (timechosen6 ==1){
-		document.getElementById("time-button-6").style.backgroundColor = "pink";
-		timechosen6 = 0;
-  }
+    reset();
+    document.getElementById("time-button-6").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 6;
+    checkIfComplete();
 }
 
 function choosetime7(){
-	if (timechosen7 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-7").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen7 = 1;
-	}
-	else if (timechosen7 ==1){
-		document.getElementById("time-button-7").style.backgroundColor = "pink";
-		timechosen7 = 0;
-  }
+    reset();
+    document.getElementById("time-button-7").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 7;
+    checkIfComplete();
 }
 
 
 function choosetime8(){
-	if (timechosen8 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-8").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen8 = 1;
-	}
-	else if (timechosen8 ==1){
-		document.getElementById("time-button-8").style.backgroundColor = "pink";
-		timechosen8 = 0;
-  }
+    reset();
+    document.getElementById("time-button-8").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 8;
+    checkIfComplete();
 }
 
 function choosetime9(){
-	if (timechosen9 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-9").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen9 = 1;
-	}
-	else if (timechosen9 ==1){
-		document.getElementById("time-button-9").style.backgroundColor = "pink";
-		timechosen8 = 0;
-  }
+    reset();
+    document.getElementById("time-button-9").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 9;
+    checkIfComplete();
 }
 
 function choosetime10(){
-	if (timechosen10 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-10").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen9 = 1;
-	}
-	else if (timechosen10 ==1){
-		document.getElementById("time-button-10").style.backgroundColor = "pink";
-		timechosen10 = 0;
-  }
+    reset();
+    document.getElementById("time-button-10").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 10;
+    checkIfComplete();
 }
 
 
 function choosetime11(){
-	if (timechosen11 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-11").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen11 = 1;
-	}
-	else if (timechosen11 ==1){
-		document.getElementById("time-button-11").style.backgroundColor = "pink";
-		timechosen11 = 0;
-  }
+    reset();
+    document.getElementById("time-button-11").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 11;
+    checkIfComplete();
 }
 
 function choosetime12(){
-	if (timechosen12 ==0 && (timechosen1 + timechosen2 + timechosen3 + timechosen4 + timechosen5 +
-    timechosen6 + timechosen7 + timechosen8 + timechosen9 + timechosen10 + timechosen11 + timechosen12) < 1){
-		document.getElementById("time-button-12").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-		timechosen12 = 1;
-	}
-	else if (timechosen12 ==1){
-		document.getElementById("time-button-12").style.backgroundColor = "pink";
-		timechosen12 = 0;
-  }
+    reset();
+    document.getElementById("time-button-12").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    timechosen = 12;
+    checkIfComplete();
+}
+
+function reset(){
+    document.getElementById("time-button-1").style.backgroundColor = "pink";
+    document.getElementById("time-button-2").style.backgroundColor = "pink";
+    document.getElementById("time-button-3").style.backgroundColor = "pink";
+    document.getElementById("time-button-4").style.backgroundColor = "pink";
+    document.getElementById("time-button-5").style.backgroundColor = "pink";
+    document.getElementById("time-button-6").style.backgroundColor = "pink";
+    document.getElementById("time-button-7").style.backgroundColor = "pink";
+    document.getElementById("time-button-8").style.backgroundColor = "pink";
+    document.getElementById("time-button-9").style.backgroundColor = "pink";
+    document.getElementById("time-button-10").style.backgroundColor = "pink";
+    document.getElementById("time-button-11").style.backgroundColor = "pink";
+    document.getElementById("time-button-12").style.backgroundColor = "pink";
+}
+
+function checkIfComplete(){
+    if ((pm != 2) & (timechosen != 0) & (timechanged != 0)){
+        var timee = document.getElementById("settime").value;
+        plchd = new Date(timee);
+        timee = plchd.getTime() + timechosen * 3600 * 1000 + pm * 12 * 3600 * 1000 - 8 * 3600 * 1000;
+        time = new Date(timee);
+        document.getElementById("inviteetime").innerHTML = time.toLocaleDateString("en-US", options);
+    }
 }
 
 function timeam(){
-  if (am == 0 && (am + pm) < 1){
+    pm = 0;
     document.getElementById("time-range-button-am").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
-    am = 1;
-  }
-  else if (am == 1){
-    document.getElementById("time-range-button-am").style.backgroundColor = "pink";
-    am = 0;
-  }
+    document.getElementById("time-range-button-pm").style.backgroundColor = "pink";
+    checkIfComplete();
 }
 
 function timepm(){
-  if (pm == 0 && (am + pm) < 1){
     document.getElementById("time-range-button-pm").style.backgroundColor = "rgb(243, 102, 114, 0.726)";
+    document.getElementById("time-range-button-am").style.backgroundColor = "pink";
     pm = 1;
-  }
-  else if (pm == 1){
-    document.getElementById("time-range-button-pm").style.backgroundColor = "pink";
-    pm = 0;
-  }
+    checkIfComplete();
+}
 
+function storetime(){
+    if (1 == 1){
+        currentchosen = localStorage.getItem("inputJSON5");
+        obj = JSON.parse(currentchosen).chosen;
+        console.log(obj);
+        if (obj == 1){
+            inputJSON = JSON.stringify({timed: time.toLocaleDateString("en-US", options)});
+            localStorage.setItem("inputJSON3", inputJSON);
+        } else if (obj == 2){
+            inputJSON = JSON.stringify({timed: time.toLocaleDateString("en-US", options)});
+            localStorage.setItem("inputJSON4", inputJSON);
+        }
+    }
 }
--- Vlog_Video.js
+++ Vlog_Video.js
@@ -25,9 +25,7 @@ function timezoneFunc() {
 	plchd = new Date(timee);
 	timee = plchd.getTime() + 8 * 3600 * 1000;
 	var time = new Date(timee);
-
 	document.getElementById("timezone1").innerHTML = "Beijing Time: " + time.toLocaleDateString("en-US" , options);
-
 }
 
 function timezoneFunc2() {
@@ -37,7 +35,28 @@ function timezoneFunc2() {
 	plchd = new Date(timee);
 	timee = plchd.getTime() + 8 * 3600 * 1000;
 	var time = new Date(timee);
-
 	document.getElementById("timezone2").innerHTML = "Beijing Time: " + time.toLocaleDateString("en-US" , options);
+}
 
+function init(){
+	time1 = localStorage.getItem("inputJSON3");
+	obj = JSON.parse(time1);
+	if (obj){
+		document.getElementById("1st").innerHTML = "<b>" + obj.timed + "</b>";
+	}
+	time2 = localStorage.getItem("inputJSON4");
+	obj = JSON.parse(time2);
+	if (obj){
+		document.getElementById("2nd").innerHTML = "<b>" + obj.timed + "</b>";
+	}
+}
+
+function storefirst(){
+	inputJSON = JSON.stringify({chosen: 1});
+	localStorage.setItem("inputJSON5", inputJSON);
+}
+
+function storesecond(){
+	inputJSON = JSON.stringify({chosen: 2});
+	localStorage.setItem("inputJSON5", inputJSON);
 }
